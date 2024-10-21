5342317b701545b706d3edcac7ab642aa8ba9efb
Daniel Martinez <daniel.martinez@healthsafe.co.nz>
2024-10-21 22:29:34 +0200

    change to gb actions

---
+++ ios/Caudales/Caudales/CaudalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaudalesApp.swift
+//  Caudales
+//
+//  Created by Daniel Martinez on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CaudalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/Caudales/CaudalesTests/CaudalesTests.swift
@@ -0,0 +1,17 @@
+//
+//  CaudalesTests.swift
+//  CaudalesTests
+//
+//  Created by Daniel Martinez on 20/10/2024.
+//
+
+import Testing
+@testable import Caudales
+
+struct CaudalesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
