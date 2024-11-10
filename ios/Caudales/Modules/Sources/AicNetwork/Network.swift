25fbb9ac9ef63569804c85a94e0fa0fd50cd8d9d
Daniel Martinez <daniel.martinez@healthsafe.co.nz>
2024-11-10 13:33:56 +0100

    iOS, added DTO models

--- ios/Caudales/Modules/Sources/AicNetwork/Network.swift
+++ ios/Caudales/Modules/Sources/AicNetwork/Network.swift
@@ -1,10 +1,10 @@
 import Foundation
 
 // MARK: - Network Error
-enum NetworkError: Error {
+public enum NetworkError: Error {
     case invalidURL
     case noData
-    case decodingError
+    case decodingError(String)
     case serverError(Int)
     case custom(String)
 }
@@ -28,73 +28,8 @@ public protocol APIEndpoint {
     var body: Data? { get }
 }
 
-// MARK: - Example API Endpoints Enum
-public enum APIEndpoints {
-    case getData(parameters: [String: String]?)
-    case postData(body: Encodable)
-    case updateData(id: String, body: Encodable)
-}
 
-// MARK: - APIEndpoint Implementation
-extension APIEndpoints: APIEndpoint {
- 
-    public var baseURL: String {
-        return "https://danitinez.github.io/" // Replace with your base URL
-    }
-    
-    public var path: String {
-        switch self {
-        case .getData:
-            return "/aic-caudales/latest.json"
-        case .postData(body: let body):
-            return "/aic-caudales"
-        case .updateData(id: let id, body: let body):
-            return "/aic-caudales"
-        }
-    }
-    
-    public var method: HTTPMethod {
-        switch self {
-        case .getData:
-            return .get
-        case .postData:
-            return .post
-        case .updateData:
-            return .put
-        }
-    }
-    
-    public var headers: [String: String]? {
-        // Common headers
-        var headers = [
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        ]
-        
-        // Add authorization if needed
-        headers["Authorization"] = "Bearer YOUR_TOKEN"
-        
-        return headers
-    }
-    
-    public var queryParameters: [String: String]? {
-        switch self {
-        case .getData(let parameters):
-            return parameters
-        default:
-            return nil
-        }
-    }
-    
-    public var body: Data? {
-        switch self {
-        case .postData(let bodyData), .updateData(_, let bodyData):
-            return try? JSONEncoder().encode(bodyData)
-        default:
-            return nil
-        }
-    }
-}
+
 
 // MARK: - Network Service
 public protocol NetworkServiceProtocol {
@@ -146,21 +81,24 @@ public class NetworkService: NetworkServiceProtocol {
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 return try decoder.decode(T.self, from: data)
+            } catch let error as DecodingError {
+                throw NetworkError.decodingError(String(describing: error))
             } catch {
-                throw NetworkError.decodingError
+                throw NetworkError.custom(error.localizedDescription)
             }
+
         default:
             throw NetworkError.serverError(httpResponse.statusCode)
         }
     }
 }
 
-// MARK: - Usage Example
-struct ExampleResponse: Codable {
-    let id: String
-    let name: String
-}
-
-struct ExampleRequest: Codable {
-    let name: String
-}
+//// MARK: - Usage Example
+//struct ExampleResponse: Codable {
+//    let id: String
+//    let name: String
+//}
+//
+//struct ExampleRequest: Codable {
+//    let name: String
+//}
